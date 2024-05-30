60ef2d8787d91cd959d51fde572986c292bdffa1
Safkiar <m.kikowski97@gmail.com>
2024-05-30 13:12:13 +0200

    finished project. Need mobile version

---
+++ src/services/supabase.js
@@ -0,0 +1,15 @@
+// import { createClient } from "@supabase/supabase-js";
+
+// export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+// const supabaseKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
+import { createClient } from "@supabase/supabase-js";
+export const supabaseUrl = "https://bapwxlrfysravotywfyr.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhcHd4bHJmeXNyYXZvdHl3ZnlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyOTUxNDAsImV4cCI6MjAzMTg3MTE0MH0.1MPGlDN6pczFMeMak389OqtZ9hs7aa13UCQIOeoHYB4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
